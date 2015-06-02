f653e6368c43659256275eec328e53f8203cadfe
Matt <matt@betterment.com>
2015-06-01 21:30:02 -0400

    cleaning

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,8 +2,5 @@ class Match < ActiveRecord::Base
   belongs_to :user, foreign_key: :created_by_user_id
   has_many :games
   has_many :game_scores, through: :games
-  has_many :users, through: :game_scores
-
-  has_one :ball_color
-  has_one :ball_quality  
+  has_many :users, through: :game_scores 
 end
