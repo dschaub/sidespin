1148058737751a2264782f198f91e9464c3086f3
Michael Romano <michael.romano@betterment.com>
2017-06-14 16:19:57 -0400

    Forgot to use operand

--- app/models/live_game.rb
+++ app/models/live_game.rb
@@ -2,14 +2,14 @@ class LiveGame < ApplicationRecord
 	belongs_to :home_user, class_name: 'User', foreign_key: :home_user_id
 	belongs_to :away_user, class_name: 'User', foreign_key: :away_user_id
 
-    scope :current, ->() { where(current: true) }
+  scope :current, ->() { where(current: true) }
 	# Maybe alias both methods (:home_elo_rating, :away_elo_rating) on :user to :elo_rating
 	# delegate :home_elo_rating, to: :home_user
 	# delegate :away_elo_rating, to: :away_user
 
-	def update_user_score(user_side, operation=:+)
+	def update_user_score(user_side, operator=:+)
 		current_score = send("#{user_side}_user_score")
-		send("#{user_side}_user_score=", current_score.send(:+, 1))
+		send("#{user_side}_user_score=", current_score.send(operator, 1))
 	end
 
 	alias_method :increment_user_score, :update_user_score
