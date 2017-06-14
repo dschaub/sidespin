f82f6ebe3dee70273896be1fbd1c8ba7fc93a1b3
Harris Effron <harris@betterment.com>
2017-06-14 16:34:47 -0400

    prevent users from creating multiple active challengs with same user

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -6,6 +6,11 @@ class Challenge < ApplicationRecord
   scope :pending, -> { where(played_at: nil, rejected_at: nil) }
 
   validate :must_not_be_played_and_rejected
+  validate :must_only_have_one_pending_challenge
+
+  def self.related_to_users(user_1, user_2)
+    find_by(home_user: user_1, away_user: user_2) || find_by(home_user: user_2, away_user: user_1)
+  end
 
   def available_opponents
     home_user.available_opponents.map do |user|
@@ -32,5 +37,11 @@ class Challenge < ApplicationRecord
 
   def must_not_be_played_and_rejected
     errors.add(:base, "Cannot play and reject the same challenge") if played_at.present? && rejected_at.present?
+  end
+
+  def must_only_have_one_pending_challenge
+    if self.class.pending.related_to_players(home_user, away_user).present?
+      errors.add(:base, "Already have active challenge")
+    end
   end
 end
