a74e50e035f33e6a25cbb6bdfef436504b16dc53
Dan Schaub <dan.s@betterment.com>
2017-06-14 14:07:22 -0400

    add elo to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
   devise :trackable, :omniauthable, :timeoutable, omniauth_providers: [:google_oauth2]
 
+  OAUTH_ALLOWED_DOMAINS = %w(betterment.com).freeze
+
+  DEFAULT_ELO = 1000
+
   has_many :outgoing_challenges, foreign_key: :home_user_id, class_name: 'Challenge'
   has_many :incoming_challenges, foreign_key: :away_user_id, class_name: 'Challenge'
 
-  OAUTH_ALLOWED_DOMAINS = %w(betterment.com).freeze
+  before_create :assign_default_elo
 
   def self.from_omniauth(auth)
     if oauth_allowed_domain? auth
@@ -29,5 +33,11 @@ class User < ApplicationRecord
     def oauth_allowed_domain?(auth)
       OAUTH_ALLOWED_DOMAINS.any? { |domain| auth.extra.raw_info.hd == domain && auth.info.email =~ /@#{domain}\z/ }
     end
+  end
+
+  private
+
+  def assign_default_elo
+    self.elo = DEFAULT_ELO
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614160739) do
+ActiveRecord::Schema.define(version: 20170614175543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20170614160739) do
     t.string "full_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "elo", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
