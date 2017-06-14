9b08701b068dab5243f28ab6545bf2ff183d15e9
Dan Schaub <dan.s@betterment.com>
2017-06-14 14:18:52 -0400

    implement elo ratings

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'simple_form'
 gem 'devise'
 gem 'omniauth-google-oauth2'
 
+gem 'elo'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/game.rb
+++ app/models/game.rb
@@ -6,6 +6,8 @@ class Game < ApplicationRecord
 
   validate :home_and_away_users_must_be_different
 
+  after_create :update_user_elos
+
   def available_opponents
     User.where.not(id: home_user.id).map do |user|
       [user.full_name, user.id]
@@ -16,5 +18,23 @@ class Game < ApplicationRecord
 
   def home_and_away_users_must_be_different
     errors.add(:base, 'Home and away users must be different') if home_user.id == away_user.id
+  end
+
+  def update_user_elos
+    home_player = Elo::Player.new(rating: home_user.elo)
+    away_player = Elo::Player.new(rating: away_user.elo)
+
+    game = home_player.versus(away_player)
+
+    if home_user_score == away_user_score
+      game.draw
+    elsif home_user_score > away_user_score
+      game.winner = home_player
+    else
+      game.winner = away_player
+    end
+
+    home_user.update!(elo: home_player.rating)
+    away_user.update!(elo: away_player.rating)
   end
 end
